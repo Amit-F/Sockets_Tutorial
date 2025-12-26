9bbac1281f2d4f0d151cd05dd172fa7d9926ae97
Amit Fink <amitfink@gmail.com>
2025-12-26 17:23:50 +0200

    Connection state recovery

--- socket-chat-example/index.js
+++ socket-chat-example/index.js
@@ -8,7 +8,9 @@ import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    connectionStateRecovery: {}
+});
 
 // app.get('/', (req, res) => {
 //     res.send('<h1>Hello world</h1>');
