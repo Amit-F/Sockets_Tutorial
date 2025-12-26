ef0e6988e5fef18184cf9a5de0d533f3f91bb8b0
Amit Fink <amitfink@gmail.com>
2025-12-26 19:10:06 +0200

    Scaling horizontally

--- socket-chat-example/index.js
+++ socket-chat-example/index.js
@@ -5,6 +5,10 @@ import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
+import { availableParallelism } from 'node:os';
+import cluster from 'node:cluster';
+import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
+
 
 // open the database file
 const db = await open({
@@ -21,71 +25,94 @@ await db.exec(`
     );
 `);
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-    connectionStateRecovery: {}
-});
-
-// app.get('/', (req, res) => {
-//     res.send('<h1>Hello world</h1>');
-// });
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
-});
-
-io.on('connection', async (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-    // socket.on('chat message', (msg) => {
-    //     // console.log('message: ' + msg);
-    //     io.emit('chat message', msg);
-    // });
-    // socket.on('chat message', async (msg) => {
-    socket.on('chat message', async (msg, clientOffset, callback) => {
-        let result;
-        try {
-            // // store the message in the database
-            // result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
-            result = await db.run(`INSERT INTO messages (content, client_offset) VALUES (?, ?)`, msg, clientOffset);
-        } catch (e) {
-            if (e.errno === 19 /* SQLITE_CONSTRAINT */) {
-                // the message was already inserted, so we notify the client (or else client will retry up to retries times)
-                callback();
-            } else {
-                // nothing to do, just let the client retry
-            }
-            return;
-        }
-
-        // include the offset with the message
-        io.emit('chat message', msg, result.lastID);
-
-        // acknowledge the event (or else client will retry up to retries times)
-        callback();
-    });
-
-    if (!socket.recovered) {
-        // if the connection state recovery was not successful
-        try {
-            await db.each('SELECT id, content FROM messages WHERE id > ?',
-                [socket.handshake.auth.serverOffset || 0],
-                (_err, row) => {
-                    socket.emit('chat message', row.content, row.id);
-                }
-            )
-        } catch (e) {
-            // something went wrong
-        }
+if (cluster.isPrimary) {
+    const numCPUs = availableParallelism();
+    // create one worker per available core
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork({
+            PORT: 3000 + i
+        });
     }
-});
 
-server.listen(3000, () => {
-    console.log('server running at http://localhost:3000/');
-});
+    // set up the adapter on the primary thread
+    setupPrimary();
+} else {
+    const app = express();
+    const server = createServer(app);
+    const io = new Server(server, {
+        connectionStateRecovery: {},
+        // set up the adapter on each worker thread
+        adapter: createAdapter()
+    });
 
+
+
+    // app.get('/', (req, res) => {
+    //     res.send('<h1>Hello world</h1>');
+    // });
+
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+
+    app.get('/', (req, res) => {
+        res.sendFile(join(__dirname, 'index.html'));
+    });
+
+    io.on('connection', async (socket) => {
+        console.log('a user connected');
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+        });
+        // socket.on('chat message', (msg) => {
+        //     // console.log('message: ' + msg);
+        //     io.emit('chat message', msg);
+        // });
+        // socket.on('chat message', async (msg) => {
+        socket.on('chat message', async (msg, clientOffset, callback) => {
+            let result;
+            try {
+                // // store the message in the database
+                // result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+                result = await db.run(`INSERT INTO messages (content, client_offset) VALUES (?, ?)`, msg, clientOffset);
+            } catch (e) {
+                if (e.errno === 19 /* SQLITE_CONSTRAINT */) {
+                    // the message was already inserted, so we notify the client (or else client will retry up to retries times)
+                    callback();
+                } else {
+                    // nothing to do, just let the client retry
+                }
+                return;
+            }
+
+            // include the offset with the message
+            io.emit('chat message', msg, result.lastID);
+
+            // acknowledge the event (or else client will retry up to retries times)
+            callback();
+        });
+
+        if (!socket.recovered) {
+            // if the connection state recovery was not successful
+            try {
+                await db.each('SELECT id, content FROM messages WHERE id > ?',
+                    [socket.handshake.auth.serverOffset || 0],
+                    (_err, row) => {
+                        socket.emit('chat message', row.content, row.id);
+                    }
+                )
+            } catch (e) {
+                // something went wrong
+            }
+        }
+    });
+
+    // each worker will listen on a distinct port
+    const port = process.env.PORT;
+
+    server.listen(port, () => {
+        console.log(`server running at http://localhost:${port}`);
+    });
+
+    // server.listen(3000, () => {
+    //     console.log('server running at http://localhost:3000/');
+    // });
+}
